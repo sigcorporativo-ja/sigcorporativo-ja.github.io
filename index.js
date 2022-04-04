a07640f769d1422182d2ea7dc08b0fc3940eadcd
fbma <fborja.manas@gmail.com>
2022-04-04 09:22:39 +0200

    Seccion noticias

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+
+
 $(document).ready(function () {
   $('#sidebarCollapse').on('click', function () {
     $('#sidebar').toggleClass('active');
   });
 
-  $("#contentppal").load("proyecto.html");
+  var hash = document.location.hash;
+  if (hash) {
+    $("#contentppal").load("noticias.html", function(){
+      $("#" + hash.substring(1)).trigger('click');
+    });
+  } else {
+    $("#contentppal").load("proyecto.html");
+  }
+
+  $(document).on('click', '.noticia', function(e) {
+    e.preventDefault(); 
+    var url = $(this).attr('contenido');
+    var id = $(this).attr('id');
+    // Solo se carga la pagina la primera vez que se accede
+    if($("#" + id + "content").is(':empty')){
+      $("#" + id + "content").load(url);
+    } 
+  });
 });
 
 $('nav li').on('click', 'a', function (e) {
@@ -13,3 +32,8 @@ $('nav li').on('click', 'a', function (e) {
     $("#contentppal").load(url);
   }
 });
+
+
+
+
+ 
